144c8406f79ebbfbf5cde42f3e2db1d45bf17789
Matias Marfisi <matimarfisi99@gmail.com>
2026-02-27 19:29:03 +0000

    feat: update setup script

--- setup_dir.sh
+++ setup_dir.sh
@@ -1,2 +1,15 @@
 #!/bin/sh
+# Inicializa el workspace de west si aún no está inicializado, y descarga
+# zephyr + módulos.
+#
+# Uso:
+#   ./setup_dir.sh
+
+SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
+
+# Si .west/config no existe en el workspace, ejecuta west init
+if ! west topdir ; then
+    west init -l "$SCRIPT_DIR"
+fi
+
 west update
